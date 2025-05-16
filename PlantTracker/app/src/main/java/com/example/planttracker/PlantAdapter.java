62f8a92fc900352657129fb698e7a18666e7bd51
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2025-05-16 17:11:32 +0300

    Adding plant adapter class

---
+++ PlantTracker/app/src/main/java/com/example/planttracker/PlantAdapter.java
@@ -0,0 +1,59 @@
+package com.example.planttracker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PlantAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Plant> plantList;
+
+    public PlantAdapter(Context context, List<Plant> plantList) {
+        this.context = context;
+        this.plantList = plantList;
+    }
+
+    @Override
+    public int getCount() {
+        return plantList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return plantList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.plant_list_item, parent, false);
+        }
+
+        Plant currentPlant = plantList.get(position);
+
+        TextView nameTextView = convertView.findViewById(R.id.plantNameTextView);
+        TextView descriptionTextView = convertView.findViewById(R.id.plantDescriptionTextView);
+        TextView waterAmountTextView = convertView.findViewById(R.id.waterAmountTextView);
+        ImageView plantImageView = convertView.findViewById(R.id.plantImageView);
+
+        nameTextView.setText(currentPlant.getName());
+        descriptionTextView.setText(currentPlant.getDescription());
+        waterAmountTextView.setText("Watered: " + currentPlant.getWaterAmount() + " ml");
+        plantImageView.setImageResource(currentPlant.getImageResId());
+
+        return convertView;
+    }
+}
+
